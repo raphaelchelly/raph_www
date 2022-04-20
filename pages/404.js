728fe6692025462bacd67dfd06f8b14ec4ece7bb
SuperRaph <raphael@fabrilab.net>
2022-04-20 19:22:06 +0200

    Adding 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,21 @@
 import Link from '@/components/Link'
-
+import Image from '@/components/Image'
 export default function FourZeroFour() {
   return (
     <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
       <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-          404
-        </h1>
+      <div>
+        <Image
+          src="/static/images/error404_rc.png"
+          alt="error 404"
+          width="150px"
+          height="150px"
+        />
+      </div>
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+          404: Sorry we couldn't find this page.
         </p>
         <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
         <Link href="/">
