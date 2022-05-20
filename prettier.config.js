6aab17a9d03a2a3a05bd7fbd1df4d767adc3c2a9
SuperRaph <raphael@fabrilab.net>
2022-05-20 15:26:49 +0200

    Small changes

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  semi: false,
+  semi: true,
   singleQuote: true,
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
   trailingComma: 'es5',
   bracketSpacing: true,
-}
+};
