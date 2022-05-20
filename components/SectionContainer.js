d9f380ae8bb936e0f0f1dde286badb0827388e9a
SuperRaph <raphael@fabrilab.net>
2022-05-20 15:30:15 +0200

    change section container

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,3 +1,5 @@
 export default function SectionContainer({ children }) {
-  return <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  return (
+    <div className="mx-auto my-auto max-w-2xl px-4 sm:px-6 xl:max-w-2xl xl:px-0">{children}</div>
+  );
 }
