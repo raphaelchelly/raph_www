9ccde44581c3e207cf07550372d799fa55c8bcea
SuperRaph <raphael@fabrilab.net>
2022-05-27 15:59:41 +0200

    fixing details

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   experimental: {
@@ -27,166 +27,166 @@ module.exports = {
       fontFamily: {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
-     colors: {
-       'background-color': '#1F2023',
-       'primary-color': '#1e90ff',
-       'primary-color-dark': '#FFFF66',
-       'spotify-green': '#1DB954',
-       primary: colors.blue,
-       gray: colors.neutral,
-     },
-     typography: (theme) => ({
-       DEFAULT: {
-         css: {
-           color: theme('colors.slate.700'),
-           a: {
-             color: theme('colors.primary-color'),
-             '&:hover': {
-               color: theme('colors.primary.600'),
-             },
-             code: { color: theme('colors.primary.400') },
-           },
-           h1: {
-             fontWeight: '700',
-             letterSpacing: theme('letterSpacing.tight'),
-             color: theme('colors.slate.900'),
-           },
-           h2: {
-             fontWeight: '700',
-             letterSpacing: theme('letterSpacing.tight'),
-             color: theme('colors.slate.900'),
-           },
-           h3: {
-             fontWeight: '600',
-             color: theme('colors.slate.900'),
-           },
-           'h4,h5,h6': {
-             color: theme('colors.slate.900'),
-           },
-           pre: {
-             backgroundColor: theme('colors.gray.800'),
-           },
-           code: {
-             color: theme('colors.gray.500'),
-             backgroundColor: theme('colors.gray.100'),
-             paddingLeft: '4px',
-             paddingRight: '4px',
-             paddingTop: '2px',
-             paddingBottom: '2px',
-             borderRadius: '0.25rem',
-           },
-           'code:before': {
-             content: 'none',
-           },
-           'code:after': {
-             content: 'none',
-           },
-           details: {
-             backgroundColor: theme('colors.gray.100'),
-             paddingLeft: '4px',
-             paddingRight: '4px',
-             paddingTop: '2px',
-             paddingBottom: '2px',
-             borderRadius: '0.25rem',
-           },
-           hr: { borderColor: theme('colors.gray.200') },
-           'ol li:before': {
-             fontWeight: '600',
-             color: theme('colors.gray.500'),
-           },
-           'ul li:before': {
-             backgroundColor: theme('colors.gray.500'),
-           },
-           strong: { color: theme('colors.gray.600') },
-           blockquote: {
-             color: theme('colors.gray.900'),
-             borderLeftColor: theme('colors.gray.200'),
-           },
-         },
-       },
-       dark: {
-         css: {
-           color: theme('colors.slate.200'), //text color for dark mode
-           a: {
-             color: theme('colors.primary-color-dark'), // links in dark mode
-             '&:hover': {
-               color: theme('colors.primary.400'),
-             },
-             code: { color: theme('colors.primary.400') },
-           },
-           h1: {
-             fontWeight: '700',
-             letterSpacing: theme('letterSpacing.tight'),
-             color: theme('colors.slate.100'),
-           },
-           h2: {
-             fontWeight: '700',
-             letterSpacing: theme('letterSpacing.tight'),
-             color: theme('colors.slate.100'),
-           },
-           h3: {
-             fontWeight: '600',
-             color: theme('colors.slate.100'),
-           },
-           'h4,h5,h6': {
-             color: theme('colors.slate.100'),
-           },
-           pre: {
-             backgroundColor: theme('colors.gray.800'),
-           },
-           code: {
-             backgroundColor: theme('colors.gray.800'),
-           },
-           details: {
-             backgroundColor: theme('colors.gray.800'),
-           },
-           hr: { borderColor: theme('colors.gray.700') },
-           'ol li:before': {
-             fontWeight: '600',
-             color: theme('colors.gray.400'),
-           },
-           'ul li:before': {
-             backgroundColor: theme('colors.gray.400'),
-           },
-           strong: { color: theme('colors.gray.100') },
-           thead: {
-             color: theme('colors.gray.100'),
-           },
-           tbody: {
-             tr: {
-               borderBottomColor: theme('colors.gray.700'),
-             },
-           },
-           blockquote: {
-             color: theme('colors.gray.100'),
-             borderLeftColor: theme('colors.gray.700'),
-           },
-         },
-       },
-     }),
-     keyframes: {
-       shrink: {
-         '0% , 100%': {
-           height: '0.75rem',
-         },
-         '50%': {
-           height: '0.375rem',
-         },
-       },
-       expand: {
-         '0% , 100%': {
-           height: '0.375rem',
-         },
-         '50%': {
-           height: '0.75rem',
-         },
-       },
-     },
-     animation: {
-       shrink: 'shrink 1.5s infinte',
-       expand: 'expand 1.5s infinte',
-     },
-   },
- },
- plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-}
+      colors: {
+        'background-color': '#1F2023',
+        'primary-color': '#1e90ff',
+        'primary-color-dark': '#1e90ff',
+        'spotify-green': '#1DB954',
+        primary: colors.blue,
+        gray: colors.neutral,
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.slate.700'),
+            a: {
+              color: theme('colors.primary-color'),
+              '&:hover': {
+                color: theme('colors.primary.600'),
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            h1: {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.slate.900'),
+            },
+            h2: {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.slate.900'),
+            },
+            h3: {
+              fontWeight: '600',
+              color: theme('colors.slate.900'),
+            },
+            'h4,h5,h6': {
+              color: theme('colors.slate.900'),
+            },
+            pre: {
+              backgroundColor: theme('colors.gray.800'),
+            },
+            code: {
+              color: theme('colors.gray.500'),
+              backgroundColor: theme('colors.gray.100'),
+              paddingLeft: '4px',
+              paddingRight: '4px',
+              paddingTop: '2px',
+              paddingBottom: '2px',
+              borderRadius: '0.25rem',
+            },
+            'code:before': {
+              content: 'none',
+            },
+            'code:after': {
+              content: 'none',
+            },
+            details: {
+              backgroundColor: theme('colors.gray.100'),
+              paddingLeft: '4px',
+              paddingRight: '4px',
+              paddingTop: '2px',
+              paddingBottom: '2px',
+              borderRadius: '0.25rem',
+            },
+            hr: { borderColor: theme('colors.gray.200') },
+            'ol li:before': {
+              fontWeight: '600',
+              color: theme('colors.gray.500'),
+            },
+            'ul li:before': {
+              backgroundColor: theme('colors.gray.500'),
+            },
+            strong: { color: theme('colors.gray.600') },
+            blockquote: {
+              color: theme('colors.gray.900'),
+              borderLeftColor: theme('colors.gray.200'),
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme('colors.slate.200'), //text color for dark mode
+            a: {
+              color: theme('colors.primary-color-dark'), // links in dark mode
+              '&:hover': {
+                color: theme('colors.primary.400'),
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            h1: {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.slate.100'),
+            },
+            h2: {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+              color: theme('colors.slate.100'),
+            },
+            h3: {
+              fontWeight: '600',
+              color: theme('colors.slate.100'),
+            },
+            'h4,h5,h6': {
+              color: theme('colors.slate.100'),
+            },
+            pre: {
+              backgroundColor: theme('colors.gray.800'),
+            },
+            code: {
+              backgroundColor: theme('colors.gray.800'),
+            },
+            details: {
+              backgroundColor: theme('colors.gray.800'),
+            },
+            hr: { borderColor: theme('colors.gray.700') },
+            'ol li:before': {
+              fontWeight: '600',
+              color: theme('colors.gray.400'),
+            },
+            'ul li:before': {
+              backgroundColor: theme('colors.gray.400'),
+            },
+            strong: { color: theme('colors.gray.100') },
+            thead: {
+              color: theme('colors.gray.100'),
+            },
+            tbody: {
+              tr: {
+                borderBottomColor: theme('colors.gray.700'),
+              },
+            },
+            blockquote: {
+              color: theme('colors.gray.100'),
+              borderLeftColor: theme('colors.gray.700'),
+            },
+          },
+        },
+      }),
+      keyframes: {
+        shrink: {
+          '0% , 100%': {
+            height: '0.75rem',
+          },
+          '50%': {
+            height: '0.375rem',
+          },
+        },
+        expand: {
+          '0% , 100%': {
+            height: '0.375rem',
+          },
+          '50%': {
+            height: '0.75rem',
+          },
+        },
+      },
+      animation: {
+        shrink: 'shrink 1.5s infinte',
+        expand: 'expand 1.5s infinte',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
