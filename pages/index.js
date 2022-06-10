50bb10c618e2f69492fb7013348e9bca5ad607e2
SuperRaph <raphael@fabrilab.net>
2022-06-10 14:42:57 +0200

    switch work to experience

--- pages/index.js
+++ pages/index.js
@@ -183,7 +183,7 @@ export default function Home({ posts }) {
         </div>
       </div>
       <div className="content mt-12 sm:mt-24">
-        <h2 className="mb-2 text-xl font-bold text-zinc-800 dark:text-white">Work</h2>
+        <h2 className="mb-2 text-xl font-bold text-zinc-800 dark:text-white">Experience</h2>
         <ul className="mt-8">
           <li className="dark:text-zinc-350 my-5 flex items-center gap-4 text-zinc-500">
             <a
