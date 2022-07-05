703f4c7a8b283cd96fddf9373ce06980154bd82a
SuperRaph <raphael@fabrilab.net>
2022-07-05 16:47:37 +0200

    GA fix

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com giscus.app;
-  style-src 'self' 'unsafe-inline' *.googleapis.com ;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com giscus.app;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src giscus.app youtube.com www.youtube.com facebook.com www.facebook.com;
 `;
 
 const securityHeaders = [
